0f7ed71adb0cda7d8c33b8659ab214f76a01fb8a
Gyan <gyanusa123@gmail.com>
2018-01-18 20:59:54 -0800

    learned about primitives objects variables

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var sentence = "asdfffg";
+console.log(sentence);
+
+var digits = 45;
+console.log(digits);
+
+var fact = true;
+console.log(fact);
+
+let firstNumber = 22;
+console.log(firstNumber);
+
+{
+    const truth = "asdfsffffff";
+console.log(truth);
+
+}
+
+const truth = "aaaaa";
+console.log(truth);
+
+let rightNow = new Date();
+console.log(rightNow);
+
+var iLikePI = Math.PI;
+console.log(iLikePI);
+
+var string = "LaLaLaLa";
+
+var newString = new String("LaLaLaLa")
+
+console.log(string);
+console.log(newString.toUperCase());
+
+
+
